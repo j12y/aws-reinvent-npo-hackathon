5ba46d1b2db915bbfea4b02e26b7de31e9617604
j12y <jayson.delancey@gmail.com>
2018-11-27 18:17:28 -0800

    donation endpoints to make policy decision

--- evaluate-image-api/donate.js
+++ evaluate-image-api/donate.js
@@ -1,15 +1,85 @@
 'use strict';
 
-module.exports.donate = async (event, context) => {
+var fs = require('fs');
+var data = fs.readFileSync("donation-guide.json", "utf8");
+var guide = JSON.parse(data);
+
+// Given an item will return determination of whether or not
+// the item can be accepted by Goodwill for donation.
+//
+// GET /donate/policy?name=Toaster
+module.exports.getPolicyDecision = async (event, context) => {
+    var query = event.queryStringParameters["item"];
+    var result = false;
+
+    if (guide["accept"].indexOf(query) >= 0) {
+        result = true;
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            accepted: result,
+            value: '0.0'
+        })
+    }
+}
+
+module.exports.healthcheck = async (event, context) => {
+    /*
+    "resource": "/donate/health",
+     "path": "/donate/health",
+     "httpMethod": "GET",
+     "headers": {...},
+     "queryStringParameters: { "Item": "Toaster"}"
+    */
+    console.log(event);
+    console.log(context);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            event: event,
+            context: context,
+        })
+    }
+}
+
+// Returns a list of all donatable items that fall into the
+// given category
+//
+// GET /donate/category?name=Clothing
+module.exports.getCategory = async (event, context) => {
+    console.log("Not implemented.");
+    return {
+        statusCode: 200,
+        body: JSON.stringify([
+            "Pants",
+            "Shirt",
+            "Hat"
+        ])
+    }
+}
+
+// Creates a new record for a donatable item
+//
+// POST /donate/item
+// {"name": "Toaster", "category": "Appliance", "value": "3.99", "aliases": []}
+module.exports.postItem = async (event, context) => {
+    console.log("Not implemented.");
+    return {
+        statusCode: 201,
+        body: JSON.stringify({})
+    }
+}
+
+// Updates a record with new details for the donation guide
+//
+// PUT /donate/item
+// {"name": "Toaster", "category": "Appliance", "value": "3.99", "aliases": ["bread warmer"]}
+module.exports.putItem = async (event, context) => {
+  console.log("Not implemented.");
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    statusCode: 204,
+    body: JSON.stringify({})
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-
